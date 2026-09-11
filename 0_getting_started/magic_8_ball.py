adaf6dd1bd0242bce03bec8f05a073ca0443e926
jaundt <jkolqqa@gmail.com>
2026-09-11 11:35:14 -0700

    Updated the script to include a loop

---
+++ 0_getting_started/magic_8_ball.py
@@ -0,0 +1,21 @@
+import random
+
+
+ans1 = ('maybe')
+ans2 = ('it is unlikely')
+ans3 = ('it is decidedly so')
+ans4 = ('definitely')
+ans5 = ('it is undecided')
+ans6 = ('as I see it, yes')
+ans7 = ('not at all')
+ans8 = ('reply hazy, try again')
+
+answers = [ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8]
+
+
+
+while True:
+    usrA = input("Ask the magic 8 ball a question: ")
+    print(random.choice(answers))
+    if input('type "quit" to end the program or leave your answer blank to ask another question: ') == 'quit':
+        break
