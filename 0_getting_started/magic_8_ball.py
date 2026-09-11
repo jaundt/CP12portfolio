5a07b6a8785cda8589b6ddecc17ca1667529f334
jaundt <jkolqqa@gmail.com>
2026-09-11 11:45:23 -0700

    Update magic_8_ball.py

--- 0_getting_started/magic_8_ball.py
+++ 0_getting_started/magic_8_ball.py
@@ -10,12 +10,15 @@ ans6 = ('as I see it, yes')
 ans7 = ('not at all')
 ans8 = ('reply hazy, try again')
 
-answers = [ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8]
+answers = [ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8]  # List of possible answers and refers to the variables above (I did this to comprehed the code)
 
 
-
+# loops the program until the user types "quit" to end the program  v
 while True:
     usrA = input("Ask the magic 8 ball a question: ")
     print(random.choice(answers))
+
+    
+    #breaks the loop if the user types "quit" or leaves the answer blank
     if input('type "quit" to end the program or leave your answer blank to ask another question: ') == 'quit':
         break
